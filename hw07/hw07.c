566534d77097ac93d83d84d5e5d62d4951f13761
DanielLin94144 <daniel094144@gmail.com>
2021-05-01 23:05:00 +0800

    finish hw07

--- hw07/hw07.c
+++ hw07/hw07.c
@@ -8,22 +8,20 @@
 #include <string.h>
 
 /* global variable declaration */
-int N;  // the number of records
 struct graph *G; // main graph would be stored in G
 struct graph *GT; // transpose of graph G
 int Vlen, Elen; // the number of vertices and edges
-int len = 13; // there are some 4-words chinese name
-char** T;  // chinese name lookup table
+int len = 13; // there are some 4-words Chinese name
+char** T;  // Chinese name lookup table
 int* visited;   // visited array (not visited: 0/ visited: 1)
 int* slist;  // list containing topological sorting result 
 int* R;  // root of forest set
 int head_idx;  // record the head index from the end of list to the beginning
 
-
 /* Structure declaration */
 struct node
 {
-    int vertex;                             // chinese name idx
+    int vertex;                             // Chinese name idx
     struct node* next;                      // point to the next node
 };
 
@@ -40,10 +38,10 @@ struct graph* createGraph(int numV);        // create graph dynamically
 // add directed edge <v1,v2> to adjacency list in graph G
 void addEdge(struct graph* G, int v1, int v2);
 void readGraph(void);       // read the input data convert to graph
-// read chinese name for each individual to chinese name lookup table T
+// read Chinese name for each individual to Chinese name lookup table T
 void ref_table(void);       
-int str2int(char* str); // convert chinese name to integer (person ID)
-char* int2str(int idx); // convert integer (person ID) to chinese name
+int str2int(char* str); // convert Chinese name to integer (person ID)
+char* int2str(int idx); // convert integer (person ID) to Chinese name
 int SCC(struct graph* G, struct graph* GT); // strongly connected component
 void topsort_call(struct graph* G);  // topsort for forest condition
 void topsort(struct graph* G, int v);  // topsort starting from vertex v
@@ -94,18 +92,20 @@ double GetTime(void)
 // and initializing the next pointer to NULL
 struct node* createNode(int v)
 {
-    // use malloc() to allocate memory in node structure
+    // allocate memory in node structure
     struct node* newNode = malloc(sizeof(struct node));
+
     newNode->vertex = v;    // store key value in "vertex" member
     newNode->next = NULL;   // initialize to NULL
     return newNode;
 }
 
-// createGraph function: 
+// createGraph: allocate memory for adjacency list and create graph 
 struct graph* createGraph(int numV)
 {
     int i;      // loop index
     struct graph* graph = malloc(sizeof(struct graph));
+
     graph->numVertices = numV;
     // allocate memory for the head pointer of adjacency list
     graph->adjList = malloc((numV) * sizeof(struct node));
@@ -117,30 +117,35 @@ struct graph* createGraph(int numV)
     return graph;
 }
 
-// // addEdge: 
+// // addEdge: add edge <v1, v2> to adjacency list in graph
 void addEdge(struct graph* G, int v1, int v2)
 {
     // v1 links to v2
     struct node* newNode = createNode(v2); // allocate memory for new node v2
+
     newNode->next = G->adjList[v1]; // new node point to adjList
     G->adjList[v1] = newNode; // update adjList
 }
 
-// readGraph: 
+// readGraph: In the readGraph function, first I call ref_table() to 
+// build reference table T[1:n], and then I create both regular graph G 
+// and transpose graph GT by createGraph() and addedge(). 
 void readGraph(void)
 {
     int i; 
     char V1[len], V2[len];  // nodes on edge (v1, v2)
     int v1, v2;
-    scanf("%d%d", &Vlen, &Elen);
 
+    // read the first line in input file
+    scanf("%d%d", &Vlen, &Elen);
+    // allocate memory for table T
     T = (char**)malloc(Vlen * sizeof(char*));
     for (i = 0; i < Vlen; i++) {
         T[i] = (char*)malloc(len * sizeof(char));
     }
-    // read chinese name for each individual
+    // read Chinese name for each individual
     ref_table();
-
+    // create both G and GT
     G = createGraph(Vlen);
     GT = createGraph(Vlen);
 
@@ -154,6 +159,8 @@ void readGraph(void)
     }
 }
 
+// ref_table:  
+// read Chinese name for each individual to Chinese name lookup table T
 void ref_table(void)
 {
     int i;      // loop index
@@ -163,6 +170,7 @@ void ref_table(void)
     }
 }
 
+// str2int: find corresponding integer ID for given Chinese name by T
 int str2int(char* str)
 {
     int i;  // loop index
@@ -175,27 +183,37 @@ int str2int(char* str)
     return -1;
 }
 
+// int2str: find Chinese name by idx by T
 char* int2str(int idx)
 {
     return T[idx];
 }
 
+// SCC: strongly connected component algorithm to find friend subgroups
+// Input: G graph for communication list, GT graph G with transpose edge, 
+// Vlen number of vertex = n, visited[1:n] n vertices visited or not,
+// slist[1:n] topologically sorted list.
+// Output: numGroup total number of subgroups, R[1:n] root of disjoint forests.
 int SCC(struct graph* G, struct graph* GT)
 {
     int numGroup;   
+
     head_idx = Vlen - 1;
-    
     // allocate memory for global array "visited", "slist" and "R"
     visited = (int*)malloc(Vlen * sizeof(int));
     slist = (int*)malloc(Vlen * sizeof(int));
     R = (int*)malloc(Vlen * sizeof(int));
 
-    topsort_call(G); // sorted list of vertex
-    numGroup = DFS_call(GT);
+    topsort_call(G); // topological sorted list of vertex in G
+    numGroup = DFS_call(GT); // find SCC in GT 
 
     return numGroup;
 }
 
+// topsort_call:
+// Input: G graph for communication list, Vlen number of vertex = n, 
+// visited[1:n] n vertices visited or not 
+// Output: slist[1:n] topologically sorted list. 
 void topsort_call(struct graph* G)
 {
     int i;  // loop index
@@ -211,13 +229,15 @@ void topsort_call(struct graph* G)
     }
 }
 
-// topsort:
-// "v": starting vertex
+// topsort: 
+// Input: G graph for communication list,  v starting vertex for topology sort,
+// head_idx records head index when topology sort, visited[1:n] 
+// Output: slist[1:n] topologically sorted list
 void topsort(struct graph* G, int v)
 {
-    if (head_idx >= 0) {
-        struct node* temp;
+    struct node* temp;
 
+    if (head_idx >= 0) {
         visited[v] = 1;
         temp = G->adjList[v];
 
@@ -225,18 +245,24 @@ void topsort(struct graph* G, int v)
             if (visited[temp->vertex] == 0) { // neighbor vertex not visited 
                 topsort(G, temp->vertex);
             }
-            temp = temp->next;
+            temp = temp->next; // update temp to next adjacent vertex
         }
-        slist[head_idx] = v;
-        head_idx--;
+        slist[head_idx] = v;  // add current vertex v to head of slist
+        head_idx--;  // update head idx
     }
 }
 
+// DFS_call: 
+// Input: GT graph for communication list,  visited[1:n],
+// slist[1:n] topologically sorted list, Vlen number of vertices.
+// Output: group_idx – 1=numGroup number of subgroups, 
+// R[1:n] root of disjoint forests
 int DFS_call(struct graph* GT)
 {
     int i;  // loop index
     int group_idx = 1;
 
+    // initialize visited and R to 0
     for (i = 0; i < Vlen; i++) {
         visited[i] = 0;
         R[i] = 0;
@@ -245,22 +271,26 @@ int DFS_call(struct graph* GT)
         // select starting vertex according to the order of slist
         if (visited[slist[i]] == 0) {
             DFS(slist[i], group_idx);
-            group_idx++;
+            group_idx++;    // update group_idx, next subgroup
         }
     }
-    return group_idx - 1;
+    return group_idx - 1; 
 }
 
+// DFS:
+// Input: GT graph for communication list,  visited[1:n],
+// Vlen number of vertices, group_idx .
+// Output: R[1:n] root of disjoint forests
 void DFS(int v, int group_idx)
 {
-    visited[v] = 1;
     struct node* temp = GT->adjList[v];
-
+    
+    visited[v] = 1;  // update to visited
     while (temp != NULL) {
         if (visited[temp->vertex] == 0) { // neighbor vertex not visited 
             DFS(temp->vertex, group_idx);
         }
-        temp = temp->next;
+        temp = temp->next; // update temp to next adjacent vertex
     }
-    R[v] = group_idx;
+    R[v] = group_idx; // record subgroup idx for vertex v
 }
